fd2e78ec0a7109ce69ad7af0513e4b55b1daac32
哦也段 <491882769@qq.com>
2020-01-27 18:13:43 +0800

    add two numbers

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"container/heap"
-	"fmt"
-)
-
 func main() {
-	slice := []uint8 {'(',')','[',']','{','}'}
-	//map1 := map[uint8]uint8 {'(':')',')':'(',
-	//						'[':']',']':'[',
-	//						'{':'}','}':'{'}
-	for _, value := range slice {
-		fmt.Printf("char : %c Value : %d\n", value, value)
-	}
-	isValid("()[]{}")
-}
 
-func isValid(s string) bool {
-	chars := new(heap.Interface)
-	for _, value := range s {
-
-	}
-	fmt.Printf("%v", chars)
-	return false
 }
