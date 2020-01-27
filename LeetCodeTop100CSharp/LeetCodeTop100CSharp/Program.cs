f575c6a3b2cae893ab4e89d4ed5c02eb8a376014
哦也段 <491882769@qq.com>
2020-01-27 18:08:56 +0800

    1、有效的括号

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(ValidParentheses.IsValid("}{"));
+            Console.Read();
+        }
+    }
+}
---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/ValidParentheses.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class ValidParentheses {
+        public static bool IsValid(string s) {
+            var chars = s.ToCharArray();
+            if ((chars.Length & 1) == 1) {
+                return false;
+            }
+            var stack = new Stack<char>();
+            foreach (var item in chars) {
+                if (stack.Count == 0) {
+                    stack.Push(item);
+                    continue;
+                }
+                switch (item) {
+                    case '(':
+                        stack.Push(item);
+                        break;
+                    case ')':
+                        if (stack.Peek() == '(') {
+                            stack.Pop();
+                        } else {
+                            stack.Push(item);
+                        }
+                        break;
+                    case '[':
+                        stack.Push(item);
+                        break;
+                    case ']':
+                        if (stack.Peek() == '[') {
+                            stack.Pop();
+                        } else {
+                            stack.Push(item);
+                        }
+                        break;
+                    case '{':
+                        stack.Push(item);
+                        break;
+                    case '}':
+                        if (stack.Peek() == '{') {
+                            stack.Pop();
+                        } else {
+                            stack.Push(item);
+                        }
+                        break;
+                }
+            }
+            if (stack.Count > 0) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
