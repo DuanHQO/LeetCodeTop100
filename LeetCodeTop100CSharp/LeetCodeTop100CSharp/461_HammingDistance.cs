57318f046daf5b3f4eca01e5752b689923e3cc98
哦也段 <491882769@qq.com>
2020-01-28 19:35:38 +0800

    461汉明距离

--- LeetCodeTop100CSharp/LeetCodeTop100CSharp/461_HammingDistance.cs
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/461_HammingDistance.cs
@@ -6,5 +6,16 @@ using System.Threading.Tasks;
 
 namespace LeetCodeTop100CSharp {
     class _461_HammingDistance {
+        public static int HammingDistance(int x, int y) {
+            var a = x ^ y;
+            var dis = 0;
+            while (a > 0) {
+                if ((a & 1) == 1) {
+                    dis++;
+                }
+                a >>= 1;
+            }
+            return dis;
+        }
     }
 }
