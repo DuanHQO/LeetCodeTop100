8f8b28b50fa065be8c30fee6714a868d8125a620
哦也段 <491882769@qq.com>
2020-01-28 19:43:33 +0800

    226翻转二叉树

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/226_InvertBinaryTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _226_InvertBinaryTree {
+
+        public static TreeNode InvertTree(TreeNode root) {
+            if (root == null) return null;
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count > 0) {
+                var node = stack.Pop();
+                if(node.right != null) {
+                    stack.Push(node.right);
+                }
+                if(node.left != null) {
+                    stack.Push(node.left);
+                }
+                var left = node.left;
+                node.left = node.right;
+                node.right = left;
+            }
+            return root;
+        }
+    }
+}
