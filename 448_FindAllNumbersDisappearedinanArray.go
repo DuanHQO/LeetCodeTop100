fa1e0eb88acd2a830e8a8b1fc0b27c52f1a3b7f9
哦也段 <491882769@qq.com>
2020-01-29 18:58:03 +0800

    448. 找到所有数组中消失的数字

---
+++ 448_FindAllNumbersDisappearedinanArray.go
@@ -0,0 +1,21 @@
+package main
+
+func findDisappearedNumbers(nums []int) []int {
+	var slice []int
+	for i := 0; i < len(nums); i++ {
+		index := nums[i]
+		if index < 0 {
+			index *= -1
+		}
+		if nums[index-1] > 0 {
+			nums[index-1] *= -1
+		}
+	}
+
+	for index, value := range nums {
+		if value > 0 {
+			slice = append(slice, index+1)
+		}
+	}
+	return slice
+}
