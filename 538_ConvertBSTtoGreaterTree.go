9919bdc511b0fc7b693c638da1a1c53202537aaa
哦也段 <491882769@qq.com>
2020-01-29 14:50:29 +0800

    把二叉搜索树转换为累加树

---
+++ 538_ConvertBSTtoGreaterTree.go
@@ -0,0 +1,24 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func convertBST(root *TreeNode) *TreeNode {
+	sum := 0
+	root = convert(root, &sum)
+	return root
+}
+
+func convert(root *TreeNode, sum *int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	convert(root.Right, sum)
+	root.Val += *sum
+	*sum = root.Val
+	convert(root.Left, sum)
+	return root
+}
