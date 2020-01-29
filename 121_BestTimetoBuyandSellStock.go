872b4ee8ec8a182019802b0f43faa8941cb8a053
哦也段 <491882769@qq.com>
2020-01-29 21:46:55 +0800

    121买卖股票的最佳时机

---
+++ 121_BestTimetoBuyandSellStock.go
@@ -0,0 +1,19 @@
+package main
+
+func maxProfit(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+
+	min := prices[0]
+	max := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < min {
+			min = prices[i]
+		}
+		if max < prices[i]-min {
+			max = prices[i] - min
+		}
+	}
+	return max
+}
