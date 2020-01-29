4e30b6668845a07e3356c52c0cfad9fe36899881
哦也段 <491882769@qq.com>
2020-01-29 11:02:19 +0800

    只出现一次的数字

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/126_SingleNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _126_SingleNumber {
+        public int SingleNumber(int[] nums) {
+            var tmp = nums[0];
+            for (int i = 1; i < nums.Length; i++) {
+                tmp ^= nums[i];
+            }
+            return tmp;
+        }
+    }
+}
