652d44e91c743b33b255e848006b77b0eb6bcd30
哦也段 <491882769@qq.com>
2020-01-29 11:54:33 +0800

    1、多数元素 2、合并两个有序链表

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/169_MajorityElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _169_MajorityElement {
+        public int MajorityElement(int[] nums) {
+            var count = 0;
+            var major = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                if(count == 0) {
+                    major = nums[i];
+                    count = 1;
+                } else {
+                    count += (nums[i] == major) ? 1 : -1;
+                }
+            }
+            return major;
+        }
+    }
+}
