eea2b1d4197ceac589d049a1e7f24c2a5710b4c9
哦也段 <491882769@qq.com>
2020-01-30 15:11:59 +0800

    414环形链表

---
+++ 141_LinkedListCycle.go
@@ -0,0 +1,18 @@
+package main
+
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+
+	//双指针，如果是环形的，快指针总能追上慢指针
+	slow := head
+	fast := slow.Next
+
+	for slow != nil && fast != nil && fast.Next != nil && slow != fast {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow == fast
+}
