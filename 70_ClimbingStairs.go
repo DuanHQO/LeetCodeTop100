1629d3bc3455a9c0e74b9f83dcd18a9eb6dc32a4
哦也段 <491882769@qq.com>
2020-01-30 17:42:34 +0800

    70爬楼梯

---
+++ 70_ClimbingStairs.go
@@ -0,0 +1,16 @@
+package main
+
+func ClimbStairs(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	i1 := 1
+	i2 := 2
+	for i := 3; i <= n; i++ {
+		temp := i1 + i2
+		i1 = i2
+		i2 = temp
+	}
+	return i2
+}
