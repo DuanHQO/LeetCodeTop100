1eedecafcc49cd2ca3a17ad9dac404afae446030
哦也段 <491882769@qq.com>
2020-02-09 22:47:01 +0800

    621任务调度器

--- algirithm/621_TaskScheduler.go
+++ algirithm/621_TaskScheduler.go
@@ -1,1 +1,39 @@
 package algirithm
+
+import "sort"
+
+func LeastInterval(tasks []byte, n int) int {
+	if tasks == nil {
+		return 0
+	}
+
+	if n == 0 {
+		return len(tasks)
+	}
+
+	time := 0
+
+	dic := make([]int, 26)
+
+	for _, task := range tasks {
+		dic[task-'A']++
+	}
+	sort.Ints(dic)
+
+	for dic[25] > 0 {
+		i := 0
+		for i <= n {
+			if dic[25] == 0 {
+				break
+			}
+			if i < 26 && dic[25-i] > 0 {
+				dic[25-i]--
+			}
+			time++
+			i++
+		}
+		sort.Ints(dic)
+	}
+
+	return time
+}
