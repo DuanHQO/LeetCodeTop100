06db4af18a8d073b447c722fb8b22fb017152365
哦也段 <491882769@qq.com>
2020-02-13 18:02:43 +0800

    33搜索旋转排序数组

---
+++ algirithm/33_SearchinRotatedSortedArray.go
@@ -0,0 +1,34 @@
+package algirithm
+
+func Search(nums []int, target int) int {
+	if nums == nil || len(nums) == 0 {
+		return -1
+	}
+
+	left := 0
+	right := len(nums) - 1
+	mid := left + (right-left)/2
+
+	for left <= right {
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target <= nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] <= target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+		mid = left + (right-left)/2
+	}
+
+	return -1
+}
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "leetcodetop100/algirithm"
+import (
+	"fmt"
+	"leetcodetop100/algirithm"
+)
 
 func main() {
 	//algirithm.CoinChange([]int{2}, 3)
 	//algirithm.CoinChange([]int{1, 2, 5}, 11)
 	//algirithm.CoinChange([]int{1,2147483647}, 2)
-	algirithm.MaxProduct([]int{2, 3, -2, 4})
+	//algirithm.MaxProduct([]int{2, 3, -2, 4})
+	fmt.Println(algirithm.Search([]int{3, 1}, 1))
+
 }
