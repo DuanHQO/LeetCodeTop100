28c51adf9bdb57440867a220f45843e17eaefe2e
哦也段 <491882769@qq.com>
2020-02-14 17:07:47 +0800

    437路径总和III

--- algirithm/437_PathSumIII.go
+++ algirithm/437_PathSumIII.go
@@ -4,31 +4,19 @@ func pathSum(root *TreeNode, sum int) int {
 	if root == nil {
 		return 0
 	}
-	return hasSum(root, sum) + pathSum(root.Left, sum) + pathSum(root.Right, sum)
+	return helperPathSum(root, sum) + pathSum(root.Left, sum) + pathSum(root.Right, sum)
 }
 
-func sumGo(root *TreeNode, sum int, count *int) {
-	if root == nil {
-		return
-	}
-	sum -= root.Val
-	if sum == 0 {
-		*count++
-	}
-	sumGo(root.Left, sum, count)
-	sumGo(root.Right, sum, count)
-}
-
-func hasSum(root *TreeNode, sum int) int {
+func helperPathSum(root *TreeNode, sum int) int {
 	if root == nil {
 		return 0
 	}
 
-	sum -= root.Val
-
-	if sum == 0 {
-		return 1 + hasSum(root.Left, sum) + hasSum(root.Right, sum)
-	} else {
-		return hasSum(root.Left, sum) + hasSum(root.Right, sum)
+	res := 0
+	if root.Val == sum {
+		res++
 	}
+	res += helperPathSum(root.Left, sum-root.Val)
+	res += helperPathSum(root.Right, sum-root.Val)
+	return res
 }
