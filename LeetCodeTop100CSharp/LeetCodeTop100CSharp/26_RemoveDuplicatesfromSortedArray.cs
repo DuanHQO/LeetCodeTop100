00be0bdd10d73314c907ce3ed4c4bde92ecda1ec
哦也段 <491882769@qq.com>
2020-02-17 18:55:04 +0800

    26、删除排序数组中的重复项

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/26_RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _26_RemoveDuplicatesfromSortedArray {
+        public int RemoveDuplicates(int[] nums) {
+            if(nums == null || nums.Length == 0) {
+                return 0;
+            }
+            var slow = 0;
+
+            for (int fast = 1; fast < nums.Length; fast++) {
+                if(nums[fast] != nums[slow]) {
+                    slow++;
+                    nums[slow] = nums[fast];
+                }
+            }
+
+            return slow + 1;
+        }
+    }
+}
