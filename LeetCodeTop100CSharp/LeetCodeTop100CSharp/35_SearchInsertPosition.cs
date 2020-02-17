4512b31c1a877049d4ca530f4acdcce6164c334f
哦也段 <491882769@qq.com>
2020-02-17 21:05:11 +0800

    35、搜索插入位置

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/35_SearchInsertPosition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _35_SearchInsertPosition {
+        public int SearchInsert(int[] nums, int target) {
+            if(nums == null || nums.Length == 0) {
+                return 0;
+            }
+
+            var lo = 0;
+            var hi = nums.Length - 1;
+            while (lo <= hi) {
+                var mid = lo + (hi - lo) / 2;
+                if (target == nums[mid]) {
+                    return mid;
+                } else if (target > nums[mid]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+
+            return lo;
+            
+        }
+    }
+}
