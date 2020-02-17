2ef626c699f1f68f6ce64316f5084d0babe50ac7
哦也段 <491882769@qq.com>
2020-02-17 20:42:31 +0800

    27、移除元素

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/27_RemoveElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _27_RemoveElement {
+        public int RemoveElement(int[] nums, int val) {
+            if(nums == null || nums.Length == 0) {
+                return 0;
+            }
+            var slow = 0;
+            for (int fast = 0; fast < nums.Length; fast++) {
+                if(nums[fast] != val) {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+            }
+            return slow;
+        }
+    }
+}
