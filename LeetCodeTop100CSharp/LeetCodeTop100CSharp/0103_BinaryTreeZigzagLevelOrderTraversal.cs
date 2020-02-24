143b721bfa1d8e78cfa38eb3a01316e9afff3088
哦也段 <491882769@qq.com>
2020-02-24 11:52:59 +0800

    103. 二叉树的锯齿形层次遍历

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0103_BinaryTreeZigzagLevelOrderTraversal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0103_BinaryTreeZigzagLevelOrderTraversal {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root) {
+            var res = new List<int[]>();
+            if (root == null) {
+                return res.ToArray();
+            }
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var count = 0;
+            while (queue.Count > 0) {
+                count++;
+                var len = queue.Count;
+                var tmpList = new List<int>();
+                var tmpStack = new Stack<int>();
+                while (len > 0) {
+                    var node = queue.Dequeue();
+                    if ((count & 1) == 1) {
+                        tmpList.Add(node.val);
+                    } else {
+                        tmpStack.Push(node.val);
+                    }
+                    if (node.left != null) {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.Enqueue(node.right);
+                    }
+                    len--;
+                }
+                if ((count & 1) == 1) {
+                    res.Add(tmpList.ToArray());
+                } else {
+                    res.Add(tmpStack.ToArray());
+                }
+            }
+
+            return res.ToArray();
+        }
+    }
+}
--- LeetCodeTop100CSharp/LeetCodeTop100CSharp/Program.cs
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/Program.cs
@@ -5,36 +5,6 @@ namespace LeetCodeTop100CSharp {
     class Program {
         static void Main(string[] args) {
 
-            var test = new int?[] { 0, 2, 4, 1, null, 3, -1, 5, 1, null, 6, null, 8};
-            var nodes = new TreeNode[test.Length];
-            for (int i = 0; i < test.Length; i++) {
-                if(test[i] != null) {
-                    nodes[i] = new TreeNode(test[i].Value);
-                }
-            }
-
-            var slow = 0;
-            var fast = 1;
-
-            while (fast < nodes.Length && fast + 1 < nodes.Length) {
-                if (nodes[slow] != null) {
-                    nodes[slow].left = nodes[fast];
-                    nodes[slow].right = nodes[fast + 1];
-                    //Console.WriteLine("node[{0}] left:{1} right:{2}", test[slow], test[fast], test[fast+1]);
-                    fast += 2;
-                }
-                slow++;
-            }
-
-            
-            foreach (var item in nodes) {
-                if (item != null) {
-                    //Console.WriteLine(item.val);
-                }
-            }
-            Console.WriteLine(_104_MaximumDepthofBinaryTree.MaxDepth(nodes[0])); 
-
-            Console.Read();
         }
     }
 
