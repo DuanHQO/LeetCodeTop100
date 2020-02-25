1e574a3e37e6cae4d0a2de85fc2736d0d9947ac9
哦也段 <491882769@qq.com>
2020-02-25 15:31:48 +0800

    144. 二叉树的前序遍历

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0144_BinaryTreePreorderTraversal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0144_BinaryTreePreorderTraversal {
+        public IList<int> PreorderTraversal(TreeNode root) {
+            var res = new List<int>();
+            if(root == null) {
+                return res.ToArray();
+            }
+
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count > 0) {
+                var node = stack.Pop();
+                res.Add(node.val);
+                if (node.right != null) {
+                    stack.Push(node.right);
+                }
+                if (node.left != null) {
+                    stack.Push(node.left);
+                }
+            }
+
+            return res.ToArray();
+        }
+    }
+}
