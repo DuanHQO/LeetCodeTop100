0b0d944fa4c0cabd3e0f2f9611024732199b6f61
哦也段 <491882769@qq.com>
2020-02-25 16:32:32 +0800

    230. 二叉搜索树中第K小的元素

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0230_KthSmallestElementinaBST.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0230_KthSmallestElementinaBST {
+        public int KthSmallest(TreeNode root, int k) {
+            var node = root;
+            var stack = new Stack<TreeNode>();
+            var list = new List<TreeNode>();
+            while (node != null || stack.Count > 0) {
+                while (node != null) {
+                    stack.Push(node);
+                    node = node.left;
+                }
+                node = stack.Pop();
+                list.Add(node);
+                node = node.right;
+            }
+            return list[k - 1].val;
+        }
+    }
+}
