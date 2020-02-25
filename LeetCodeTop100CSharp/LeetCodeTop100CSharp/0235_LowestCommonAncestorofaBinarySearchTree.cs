003128436266ff9364779c1acad6522e884e99c8
哦也段 <491882769@qq.com>
2020-02-25 17:30:56 +0800

    235. 二叉搜索树的最近公共祖先

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0235_LowestCommonAncestorofaBinarySearchTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0235_LowestCommonAncestorofaBinarySearchTree {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            var pVal = p.val;
+            var qVal = q.val;
+            var node = root;
+            while (node != null) {
+                if(pVal < node.val && pVal < node.val) {
+                    node = node.left;
+                }else if(pVal > node.val && qVal > node.val) {
+                    node = node.right;
+                } else {
+                    return node;
+                }
+            }
+            return null;
+        }
+    }
+}
