2fc101495e6c8608f90e7d06bf933b693143e4b4
哦也段 <491882769@qq.com>
2020-02-25 19:39:14 +0800

    404. 左叶子之和

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0404_SumofLeftLeaves.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0404_SumofLeftLeaves {
+        public int SumOfLeftLeaves(TreeNode root) {
+            var node = root;
+            var stack = new Stack<TreeNode>();
+            var res = 0;
+            while (node != null || stack.Count > 0) {
+                while (node != null) {
+                    stack.Push(node);
+                    node = node.left;
+                }
+                node = stack.Pop();
+                if (node.left != null && node.left.left == null && node.left.right == null)
+                    res += node.left.val;
+                node = node.right;
+            }
+
+            return res;
+        }
+    }
+}
