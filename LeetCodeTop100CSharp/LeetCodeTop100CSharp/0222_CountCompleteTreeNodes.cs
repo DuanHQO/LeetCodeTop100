096121c88ac368d00575bdfa86fbb0ceef18ae44
哦也段 <491882769@qq.com>
2020-02-25 16:26:16 +0800

    222. 完全二叉树的节点个数

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0222_CountCompleteTreeNodes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0222_CountCompleteTreeNodes {
+        public int CountNodes(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            var res = 0;
+            var result = new Queue<int>();
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count > 0) {
+                var node = stack.Pop();
+                res++;
+                if (node.right != null)
+                    stack.Push(node.right);
+                if (node.left != null)
+                    stack.Push(node.left);
+            }
+            return res;
+        }
+    }
+}
