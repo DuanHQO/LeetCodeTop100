d8df0774f507e23530de03084f8b633ab7c79a8d
哦也段 <491882769@qq.com>
2020-02-25 16:11:42 +0800

    199. 二叉树的右视图

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0199_BinaryTreeRightSideView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0199_BinaryTreeRightSideView {
+        public IList<int> RightSideView(TreeNode root) {
+            var res = new Queue<int>();
+            if(root == null) {
+                return res.ToArray();
+            }
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0) {
+                var len = queue.Count;
+                while (len > 0) {
+                    var node = queue.Dequeue();
+                    if(len == 1) {
+                        res.Enqueue(node.val);
+                    }
+                    if(node.left!= null) {
+                        queue.Enqueue(node.left);
+                    }
+                    if(node.right != null) {
+                        queue.Enqueue(node.right);
+                    }
+                    len--;
+                }
+            }
+
+            return res.ToArray();
+        }
+    }
+}
