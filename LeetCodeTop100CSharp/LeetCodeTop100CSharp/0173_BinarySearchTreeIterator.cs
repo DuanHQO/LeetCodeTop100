3335440788c8812f3489e54dfa7a0d4e9bb0c428
哦也段 <491882769@qq.com>
2020-02-25 16:03:02 +0800

    173. 二叉搜索树迭代器

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0173_BinarySearchTreeIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0173_BinarySearchTreeIterator {
+        
+    }
+
+    public class BSTIterator {
+
+        Queue<int> queue;
+
+        public BSTIterator(TreeNode root) {
+            queue = new Queue<int>();
+            var stack = new Stack<TreeNode>();
+            var node = root;
+            while (node != null || stack.Count > 0) {
+                while (node != null) {
+                    stack.Push(node);
+                    node = node.left;
+                }
+                node = stack.Pop();
+                queue.Enqueue(node.val);
+                node = node.right;
+            }
+        }
+
+        /** @return the next smallest number */
+        public int Next() {
+            return queue.Dequeue();
+        }
+
+        /** @return whether we have a next smallest number */
+        public bool HasNext() {
+            return queue.Count > 0;
+        }
+    }
+}
