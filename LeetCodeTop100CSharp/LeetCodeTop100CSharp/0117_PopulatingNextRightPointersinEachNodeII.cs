3fb58f47e00ccc5f680442cfe9e45d7195814952
哦也段 <491882769@qq.com>
2020-02-25 15:14:54 +0800

    117. 填充每个节点的下一个右侧节点指针 II

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0117_PopulatingNextRightPointersinEachNodeII.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0117_PopulatingNextRightPointersinEachNodeII {
+        public Node Connect(Node root) {
+            if (root == null) {
+                return null;
+            }
+
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
+            while (queue.Count > 0) {
+                var len = queue.Count;
+                while (len > 0) {
+                    var node = queue.Dequeue();
+                    if (len > 1) {
+                        var next = queue.Peek();
+                        node.next = next;
+                    }
+                    if (node.left != null) {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.Enqueue(node.right);
+                    }
+                    len--;
+                }
+            }
+
+            return root;
+        }
+    }
+}
