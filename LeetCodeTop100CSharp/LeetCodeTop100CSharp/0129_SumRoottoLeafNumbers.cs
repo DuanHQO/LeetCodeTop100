a4aa2820c1a5c3fceb4dbf31bff7e55944f3f014
哦也段 <491882769@qq.com>
2020-02-25 15:23:37 +0800

    129. 求根到叶子节点数字之和

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0129_SumRoottoLeafNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0129_SumRoottoLeafNumbers {
+        public int SumNumbers(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            var res = 0;
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0) {
+                var len = queue.Count;
+                while (len > 0) {
+                    var node = queue.Dequeue();
+                    if(node.left != null) {
+                        node.left.val += node.val * 10;
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null) {
+                        node.right.val += node.val * 10;
+                        queue.Enqueue(node.right);
+                    }
+                    if(node.left == null && node.right == null) {
+                        res += node.val;
+                    }
+                    len--;
+                }
+            }
+
+            return res;
+        }
+    }
+}
