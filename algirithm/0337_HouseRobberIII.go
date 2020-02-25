64cbbaa4e092e6734df639e8d62905f1e9d06cfc
哦也段 <491882769@qq.com>
2020-02-25 19:27:03 +0800

    337打家劫舍III

--- algirithm/0337_HouseRobberIII.go
+++ algirithm/0337_HouseRobberIII.go
@@ -5,21 +5,24 @@ import (
 )
 
 func Rob(root *TreeNode) int {
-	result := robInternal(root)
-	return kit.Max(result[0], result[1])
-}
 
-func robInternal(root *TreeNode) []int {
-	if root == nil {
-		return make([]int, 2)
+	var helper func(node *TreeNode) []int
+	helper = func(node *TreeNode) []int {
+		if node == nil {
+			return make([]int, 2)
+		}
+
+		result := make([]int, 2)
+		left := helper(node.Left)
+		right := helper(node.Right)
+
+		result[0] = kit.Max(left[0], left[1]) + kit.Max(right[0], right[1])
+		result[1] = left[0] + right[0] + node.Val
+
+		return result
 	}
 
-	result := make([]int, 2)
-	left := robInternal(root.Left)
-	right := robInternal(root.Right)
+	res := helper(root)
 
-	result[0] = kit.Max(left[0], left[1]) + kit.Max(right[0], right[1])
-	result[1] = left[0] + right[0] + root.Val
-
-	return result
+	return kit.Max(res[0], res[1])
 }
