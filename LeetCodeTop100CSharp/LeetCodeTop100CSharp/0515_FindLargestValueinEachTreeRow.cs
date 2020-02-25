5b89179b2653709084da2378940ab841ce6b6a97
哦也段 <491882769@qq.com>
2020-02-25 21:19:01 +0800

    515. 在每个树行中找最大值

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0515_FindLargestValueinEachTreeRow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0515_FindLargestValueinEachTreeRow {
+        public IList<int> LargestValues(TreeNode root) {
+            var res = new List<int>();
+            if (root == null)
+                return res.ToArray();
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0) {
+                var len = queue.Count;
+                var max = queue.Peek().val;
+                while (len > 0) {
+                    var node = queue.Dequeue();
+                    max = Math.Max(node.val, max);
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+                    if (len == 1)
+                        res.Add(max);
+                    len--;
+                }
+            }
+
+
+            return res.ToArray();
+        }
+    }
+}
