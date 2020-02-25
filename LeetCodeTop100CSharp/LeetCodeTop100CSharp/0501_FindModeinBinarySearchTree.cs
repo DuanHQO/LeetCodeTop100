cc154fe9e8d0f0b01c9f92960e2f30f1c03609dc
哦也段 <491882769@qq.com>
2020-02-25 21:07:21 +0800

    501. 二叉搜索树中的众数

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/0501_FindModeinBinarySearchTree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp {
+    class _0501_FindModeinBinarySearchTree {
+        List<int> res = new List<int>();
+        int maxTimes = 0;
+        int thisTimes = 0;
+        TreeNode pre = null;
+        public int[] FindMode(TreeNode root) {
+            Inorder(root);
+            return res.ToArray();
+        }
+
+        public void Inorder(TreeNode node) {
+            if (node == null)
+                return;
+            Inorder(node.left);
+            if (pre != null && node.val == pre.val)
+                thisTimes++;
+            else
+                thisTimes = 1;
+            if (thisTimes == maxTimes) {
+                res.Add(node.val);
+            } else if (thisTimes > maxTimes) {
+                maxTimes = thisTimes;
+                res.Clear();
+                res.Add(node.val);
+            }
+            pre = node;
+            Inorder(node.right);
+        }
+    }
+}
