944fb6f7e681a920b497c7d776deed58e0e6b312
哦也段 <491882769@qq.com>
2020-03-01 17:30:45 +0800

    面试题54. 二叉搜索树的第k大节点

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0054_二叉搜索树的第k大节点.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0054_二叉搜索树的第k大节点 {
+        public int KthLargest(TreeNode root, int k) {
+            var res = new List<int>();
+            var stack = new Stack<TreeNode>();
+            var node = root;
+            while (node != null || stack.Count > 0) {
+                while (node != null) {
+                    stack.Push(node);
+                    node = node.left;
+                }
+                node = stack.Pop();
+                res.Add(node.val);
+                node = node.right;
+            }
+            return res[res.Count - k];
+        }
+    }
+}
