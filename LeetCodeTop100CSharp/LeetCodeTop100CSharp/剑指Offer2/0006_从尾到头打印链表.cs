1461cad61d5b5cbc8b454790f512023d687bdbfc
哦也段 <491882769@qq.com>
2020-03-01 15:22:34 +0800

    面试题06. 从尾到头打印链表

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0006_从尾到头打印链表.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0006_从尾到头打印链表 {
+        public int[] ReversePrint(ListNode head) {
+            var res = new List<int>();
+            if (head == null)
+                return res.ToArray();
+
+            var back = head;
+            ListNode pre = null;
+            while (back != null) {
+                var next = back.next;
+                back.next = pre;
+                pre = back;
+                back = next;
+            }
+
+            while (pre != null) {
+                res.Add(pre.val);
+                pre = pre.next;
+            }
+
+            return res.ToArray();
+        }
+    }
+}
