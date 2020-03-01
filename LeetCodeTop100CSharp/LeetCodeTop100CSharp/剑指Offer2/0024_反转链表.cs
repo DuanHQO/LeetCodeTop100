da4d7cca8cff74cab9c5166ef20d6b479995529c
哦也段 <491882769@qq.com>
2020-03-01 15:26:08 +0800

    面试题24. 反转链表

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0024_反转链表.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0024_反转链表 {
+        public ListNode ReverseList(ListNode head) {
+            var res = new List<int>();
+            if (head == null)
+                return null;
+
+            ListNode pre = null;
+            while (head != null) {
+                var next = head.next;
+                head.next = pre;
+                pre = head;
+                head = next;
+            }
+
+            return pre;
+
+        }
+    }
+}
