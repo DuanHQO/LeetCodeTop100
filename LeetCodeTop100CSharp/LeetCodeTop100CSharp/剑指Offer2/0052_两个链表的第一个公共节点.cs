abbc5ac8d15b5983a0a596ad4c2b200423a0b090
哦也段 <491882769@qq.com>
2020-03-01 20:54:49 +0800

    面试题52. 两个链表的第一个公共节点

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0052_两个链表的第一个公共节点.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0052_两个链表的第一个公共节点 {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+            if (headA == null || headB == null)
+                return null;
+
+            var map = new Dictionary<ListNode, ListNode>();
+            ListNode pre = new ListNode(-1);
+            ListNode back = new ListNode(-1);
+            pre.next = headA;
+            while (headA != null) {
+                if (!map.ContainsKey(headA)) {
+                    map.Add(headA, pre);
+                }
+                pre = headA;
+                headA = headA.next;
+            }
+            while (headB != null) {
+                if (!map.ContainsKey(headB)) {
+                    map.Add(headB, pre);
+                } else {
+                    if (map[headB] != back) {
+                        return headB;
+                    }
+                }
+                back = headB;
+                headB = headB.next;
+            }
+            return null;
+        }
+    }
+}
