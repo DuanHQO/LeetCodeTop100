5563b892685939abce09980c2073a2f8f066959f
哦也段 <491882769@qq.com>
2020-03-01 15:30:22 +0800

    面试题25. 合并两个排序的链表

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0025_合并两个排序的链表.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0025_合并两个排序的链表 {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+            var head = new ListNode(-1);
+            var pre = head;
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    head.next = l1;
+                    l1 = l1.next;
+                } else {
+                    head.next = l2;
+                    l2 = l2.next;
+                }
+                head = head.next;
+            }
+
+            if (l1 == null)
+                head.next = l2;
+            else
+                head.next = l1;
+
+            return pre.next;
+        }
+    }
+}
