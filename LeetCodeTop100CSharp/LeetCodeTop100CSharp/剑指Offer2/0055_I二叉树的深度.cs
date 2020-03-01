ac84a42307f42e7caf89d90288ce9d2b34580651
哦也段 <491882769@qq.com>
2020-03-01 15:16:34 +0800

    面试题55 - I. 二叉树的深度

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0055_I二叉树的深度.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0055_I二叉树的深度 {
+        public int MaxDepth(TreeNode root) {
+            if (root == null) return 0;
+
+            var height = 0;
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0) {
+                var len = queue.Count;
+                while (len > 0) {
+                    var node = queue.Dequeue();
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+                    len--;
+                }
+                height++;
+            }
+            return height;
+        }
+    }
+}
