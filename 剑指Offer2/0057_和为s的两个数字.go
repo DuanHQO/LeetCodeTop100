db1e90387073f8e6a8f0f73ec34052d01c10c633
哦也段 <491882769@qq.com>
2020-03-01 20:09:34 +0800

    面试题57. 和为s的两个数字

---
+++ 剑指Offer2/0057_和为s的两个数字.go
@@ -0,0 +1,22 @@
+package 剑指Offer2
+
+func twoSum(nums []int, target int) []int {
+	if nums == nil || nums[0] > target {
+		return nil
+	}
+
+	hi := len(nums) - 1
+	lo := 0
+
+	for lo < hi {
+		if nums[lo]+nums[hi] > target {
+			hi--
+		} else if nums[lo]+nums[hi] < target {
+			lo++
+		} else {
+			return []int{nums[i], nums[j]}
+		}
+	}
+
+	return nil
+}
