5ca77b73c6b4328f9f7c6c2400b6668839276a43
哦也段 <491882769@qq.com>
2020-03-03 12:44:49 +0800

    在排序数组中查找数字 I

--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import "leetcodetop100/剑指Offer2"
 
 func main() {
 	//algirithm.NthUglyNumber(10)
-	剑指Offer2.Merge0010([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3)
+	剑指Offer2.Search0053([]int{5, 7, 7, 8, 8, 10}, 8)
 }
---
+++ 剑指Offer2/0053_在排序数组中查找数字I.go
@@ -0,0 +1,50 @@
+package 剑指Offer2
+
+import "fmt"
+
+func Search0053(nums []int, target int) int {
+	if nums == nil {
+		return 0
+	}
+
+	ret := 0
+
+	lo := 0
+	hi := len(nums) - 1
+
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		fmt.Println(mid)
+		if nums[mid] < target {
+			lo = mid + 1
+		} else if nums[mid] > target {
+			hi = mid - 1
+		} else {
+			ret++
+			a := mid - 1
+			b := mid + 1
+
+			for a >= 0 {
+				if nums[a] == target {
+					ret++
+					a--
+				} else {
+					break
+				}
+			}
+
+			for b <= len(nums)-1 {
+				if nums[b] == target {
+					ret++
+					b++
+				} else {
+					break
+				}
+			}
+
+			break
+		}
+	}
+
+	return ret
+}
