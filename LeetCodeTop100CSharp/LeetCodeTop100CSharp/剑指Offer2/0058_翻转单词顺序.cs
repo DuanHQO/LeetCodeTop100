2fd1ba53f4f686bbce7a4eeb5dd78506ac7e65c2
哦也段 <491882769@qq.com>
2020-03-03 18:31:02 +0800

    面试题58 - I. 翻转单词顺序

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0058_翻转单词顺序.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0058_翻转单词顺序 {
+        public static string ReverseWords(string s) {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            var arr = s.Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+            var revarr = arr.Reverse();
+            return string.Join(" ", revarr);
+        }
+
+        public static void Main(string[] args) {
+            ReverseWords("the sky is blue");
+            Console.Read();
+        }
+    }
+}
