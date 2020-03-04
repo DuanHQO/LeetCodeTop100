a63f37571d3c35413b46f846c3c6a01a43eb70b0
哦也段 <491882769@qq.com>
2020-03-04 14:49:14 +0800

    面试题56 - II. 数组中数字出现的次数 II

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0056_数组中数字出现的次数 II.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0056_数组中数字出现的次数_II {
+        public int SingleNumber(int[] nums) {
+
+            var map = new Dictionary<int, int>();
+
+            foreach (var item in nums) {
+                if (map.ContainsKey(item))
+                    map[item]++;
+                else
+                    map.Add(item, 1);
+            }
+
+            foreach (var item in map) {
+                if (item.Value == 1)
+                    return item.Key;
+            }
+
+            return 0;
+        }
+    }
+}
