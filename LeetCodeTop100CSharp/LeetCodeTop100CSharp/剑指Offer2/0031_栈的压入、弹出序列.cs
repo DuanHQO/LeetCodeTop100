10e946284e1a263d6d35f57130db96a25d1d526e
哦也段 <491882769@qq.com>
2020-03-04 20:15:01 +0800

    面试题31. 栈的压入、弹出序列

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0031_栈的压入、弹出序列.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0031_栈的压入_弹出序列 {
+        public bool ValidateStackSequences(int[] pushed, int[] popped) {
+            var len = pushed.Length;
+            var j = 0;
+            var stack = new Stack<int>();
+            for (int i = 0; i < len; i++) {
+                stack.Push(pushed[i]);
+                while (stack.Count > 0 && stack.Peek() == popped[j]) {
+                    j++;
+                    stack.Pop();
+                }
+            }
+
+            return len == j;
+        }
+    }
+}
