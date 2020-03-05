ef3066a8db16909bcb7ccd2b99f047a067017a0e
哦也段 <491882769@qq.com>
2020-03-05 20:54:13 +0800

    Console.WriteLine(string.Join("", numstr));

---
+++ LeetCodeTop100CSharp/LeetCodeTop100CSharp/剑指Offer2/0045_把数组排成最小的数.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTop100CSharp.剑指Offer2 {
+    class _0045_把数组排成最小的数 {
+        public static string MinNumber(int[] nums) {
+            if (nums == null || nums.Length == 0) {
+                return string.Empty;
+            }
+
+            var numstr = new string[nums.Length];
+            for (int i = 0; i < nums.Length; i++) {
+                numstr[i] = nums[i].ToString();
+            }
+
+            for (int i = 0; i < nums.Length; i++) {
+                for (int j = i + 1; j < nums.Length; j++) {
+                    var sum = numstr[i] + numstr[j];
+                    if (string.Compare(sum, numstr[j] + numstr[i]) > 0){
+                        var tmps = numstr[i];
+                        numstr[i] = numstr[j];
+                        numstr[j] = tmps;
+                    }
+                }
+            }
+            return string.Join("", numstr);
+        }
+
+        public static void Main(string[] args) {
+            MinNumber(new int[] { 3, 30, 34, 5, 9 });
+            Console.Read();
+        }
+    }
+}
