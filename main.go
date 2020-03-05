cf528d42a71bbd16878b512e20b7db217cc870a7
哦也段 <491882769@qq.com>
2020-03-06 00:16:09 +0800

    面试题38. 字符串的排列

--- main.go
+++ main.go
@@ -4,5 +4,5 @@ import "leetcodetop100/剑指Offer2"
 
 func main() {
 	//algirithm.NthUglyNumber(10)
-	剑指Offer2.DistributeCandies(10, 3)
+	剑指Offer2.Permutation("abc")
 }
---
+++ 剑指Offer2/0038_字符串的排列.go
@@ -0,0 +1,49 @@
+package 剑指Offer2
+
+import (
+	"fmt"
+	"sort"
+)
+
+func Permutation(s string) []string {
+	var res []string
+	if s == "" {
+		return res
+	}
+
+	char := chars(s)
+
+	sort.Sort(char)
+
+	var helper func(s []byte, tmp []byte)
+	helper = func(s []byte, tmp []byte) {
+		if len(tmp) == len(char) {
+			slice := make([]byte, len(tmp), len(tmp))
+			copy(slice, tmp)
+			res = append(res, string(slice))
+			return
+		}
+		//for _, value := range start {
+		//	fmt.Printf("%c", value)
+		//}
+		//fmt.Println()
+
+		//marked := make([]bool, len(char))
+		for i := 0; i < len(s); i++ {
+			if i > 0 && s[i] == s[i-1] {
+				continue
+			}
+			//marked[i] = true
+			tmp = append(tmp, s[i])
+			ss := make([]byte, len(s), len(s))
+			copy(ss, s)
+			helper(append(ss[:i], ss[i+1:]...), tmp)
+			tmp = tmp[:len(tmp)-1]
+		}
+
+	}
+
+	helper(char, []byte{})
+	fmt.Println(res)
+	return res
+}
