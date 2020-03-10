b35dcaa6b482b47eae47de5774290cd464ea4395
哦也段 <491882769@qq.com>
2020-03-10 10:01:38 +0800

    0543、二叉树的直径

---
+++ 每日1题-3月/0543_二叉树的直径.go
@@ -0,0 +1,34 @@
+package 每日1题_3月
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	ret := 0
+
+	var max = func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	var helper func(node *TreeNode) int
+	helper = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+
+		left := helper(node.Left)
+		right := helper(node.Right)
+
+		ret = max(ret, left+right)
+		height := max(left, right) + 1
+		return height
+	}
+
+	helper(root)
+
+	return ret
+}
