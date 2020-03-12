3dbe12e98c931369fd52fb4a0d0fefa69766ef05
哦也段 <491882769@qq.com>
2020-03-12 11:56:10 +0800

    1071. 字符串的最大公因子

---
+++ 每日1题-3月/1071_字符串的最大公因子.go
@@ -0,0 +1,17 @@
+package 每日1题_3月
+
+func gcdOfStrings(str1 string, str2 string) string {
+	if str1+str2 != str2+str1 {
+		return ""
+	}
+
+	var gcd func(a, b int) int
+	gcd = func(a, b int) int {
+		if b == 0 {
+			return a
+		}
+		return gcd(b, a%b)
+	}
+
+	return str1[0:gcd(len(str1), len(str2))]
+}
