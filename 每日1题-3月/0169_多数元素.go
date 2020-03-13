f6ae6b78533f9e38f15cd071138ca64a1d5b50bc
哦也段 <491882769@qq.com>
2020-03-13 18:19:08 +0800

    多数元素

---
+++ 每日1题-3月/0169_多数元素.go
@@ -0,0 +1,19 @@
+package 每日1题_3月
+
+func majorityElement(nums []int) int {
+	num := nums[0]
+	count := 1
+	for i := 1; i < len(nums); i++ {
+		if num == nums[i] {
+			count++
+		} else {
+			count--
+			if count == 0 {
+				num = nums[i]
+				count = 1
+			}
+		}
+	}
+
+	return num
+}
