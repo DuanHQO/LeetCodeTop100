5862490fbfee33340703acdd3030aa0ca1a76019
哦也段 <491882769@qq.com>
2020-03-16 13:13:40 +0800

    1、新增目录 2、字符串解码

---
+++ Tencent/constant.go
@@ -0,0 +1,20 @@
+package 每日1题3月
+
+import "leetcodetop100/kit"
+
+type TreeNode = kit.TreeNode
+type ListNode = kit.ListNode
+
+type chars []byte
+
+func (v chars) Len() int {
+	return len(v)
+}
+
+func (v chars) Less(i, j int) bool {
+	return v[i] < v[j]
+}
+
+func (v chars) Swap(i, j int) {
+	v[i], v[j] = v[j], v[i]
+}
