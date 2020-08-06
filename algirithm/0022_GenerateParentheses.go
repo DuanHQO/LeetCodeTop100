65660c0de87337e49c50fedf33e3c2b50b1ae228
oyeduan <491882769@qq.com>
2020-08-06 10:05:06 +0800

    括号生成练习

--- algirithm/0022_GenerateParentheses.go
+++ algirithm/0022_GenerateParentheses.go
@@ -7,7 +7,23 @@ func GenerateParenthesis(n int) []string {
 		return res
 	}
 
-	dfs22(n, n, "", &res)
+	var dfs func(int, int, string, *[]string)
+
+	dfs = func(left int, right int, curr string, res *[]string) {
+		if left == 0 && right == 0 {
+			*res = append(*res, curr)
+			return
+		}
+		if left > 0 {
+			dfs(left-1, right, curr+"(", res)
+		}
+
+		if right > 0 && right > left {
+			dfs(left, right-1, curr+")", res)
+		}
+	}
+
+	dfs(n, n, "", &res)
 
 	return res
 }
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "leetcodetop100/每日1题3月"
-import _ "github.com/chai2010/errors"
+import (
+	"fmt"
+	_ "github.com/chai2010/errors"
+)
 import _ "github.com/nosixtools/timewheel"
 import _ "github.com/rfyiamcool/go-timewheel"
+import "leetcodetop100/algirithm"
 
 func main() {
-	每日1题3月.HasGroupsSizeX([]int{1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3})
+	//每日1题3月.HasGroupsSizeX([]int{1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3})
+	res := algirithm.GenerateParenthesis(3)
+	for _, re := range res {
+		fmt.Println(re)
+	}
 }
